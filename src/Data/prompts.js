8ee047576f3ffe04b2de37193242172e8ab5003c
Saikumar Chunduru <114095978+Saik2003@users.noreply.github.com>
2023-11-05 01:49:10 +0530

    Add files via upload

---
+++ src/Data/prompts.js
@@ -0,0 +1,48 @@
+export const prompts = [
+    {
+      id: 1,
+      actualMessage: "Explain the question as if I'm new to it. If possible, provide examples to illustrate.",
+      displayMessage: "Explain the question"
+    },
+    {
+      id: 2,
+      actualMessage: "Could you provide a hint on how to begin solving this question? I'm looking for a clue about the most straightforward approach, not the best solution.",
+      displayMessage: "Give me a hint on how to start solving this question"
+    },
+    {
+      id: 3,
+      actualMessage: "I've identified the brute-force solution. Can you offer a hint on optimizing it for a better solution? I only need a hint, not the complete solution.",
+      displayMessage: "How can I optimize my solution?"
+    },
+    {
+      id: 4,
+      actualMessage: "List various methods for solving this question.",
+      displayMessage: "List all solution approaches"
+    },
+    {
+      id: 5,
+      actualMessage: "What's the time complexity of the most efficient solution? Just provide the time complexity, no need for an explanation of the approach or solution.",
+      displayMessage: "What is the expected optimal time complexity?"
+    },
+    {
+      id: 6,
+      actualMessage: "Could you suggest a potential interview question based on this problem?",
+      displayMessage: "Ask me a potential interview question"
+    },
+    {
+      id: 7,
+      actualMessage: "Enumerate questions that share similarities with the concepts used in this problem.",
+      displayMessage: "List similar questions"
+    },
+    {
+      id: 8,
+      actualMessage: "Can you point out the issues in my code for this question?",
+      displayMessage: "Tell me what's wrong with my code"
+    },
+    {
+      id: 9,
+      actualMessage: "Assume the role of an interviewer. I'll be the candidate, and you'll ask me interview questions. You can reference the current question if needed. Please respond as an interviewer, asking questions one at a time and waiting for my answers. Avoid providing explanations; just conduct the interview.",
+      displayMessage: "Be my interviewer"
+    }
+  ];
+  
